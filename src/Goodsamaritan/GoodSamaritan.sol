d0dbe68da1cb3a6f6b5a889bbb1f6cd66b321418
hark017 <harendrachaudhary1712@gmail.com>
2026-02-04 20:03:11 +0530

    asa

--- src/Goodsamaritan/GoodSamaritan.sol
+++ src/Goodsamaritan/GoodSamaritan.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.0 <0.9.0;
 
-import "@openzeppelin/contracts/utils/address.sol";
+import "../helpers/address.sol";
 
 contract GoodSamaritan {
     Wallet public wallet;
